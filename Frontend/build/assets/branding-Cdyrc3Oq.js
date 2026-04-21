6d0f0ba2a82734e678c18012ca391262548720a0
anup soni <sonianup8965@gmail.com>
2026-04-22 03:55:47 +0530

    hello

---
+++ Frontend/build/assets/branding-Cdyrc3Oq.js
@@ -0,0 +1,1 @@
+function e(){return`© ${new Date().getFullYear()} Ansoyal AI. All rights reserved.`}export{e as i};
---
+++ Frontend/build/assets/premiumtemplate-vpa3Tkf2.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-CUHgHkgj.js";import{S as m}from"./StyleTemplateHub-CP7HJOnh.js";import"./index-B1YomhOd.js";import"./motion-fybuDPvH.js";import"./graphics-CE2DZ1dB.js";import"./AppHeader-3t2Gn64F.js";import"./AppFooter-DyUcxGZ3.js";import"./branding-Cdyrc3Oq.js";function l(){return t.jsx(m,{styleId:"premium"})}export{l as default};
