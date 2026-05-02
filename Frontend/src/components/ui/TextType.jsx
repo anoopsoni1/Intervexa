f40dd3829348f12db3570fd6b3d830188540227b
anup soni <sonianup8965@gmail.com>
2026-05-02 22:08:24 +0530

    hello

--- Frontend/src/components/ui/TextType.jsx
+++ Frontend/src/components/ui/TextType.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState, createElement, useMemo, useCallback } from 'react';
-import { gsap } from 'gsap';
+import gsap from "gsap";
 
 const TextType = ({
   text,
